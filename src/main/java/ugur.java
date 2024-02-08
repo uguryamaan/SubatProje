a31c212bfa4bd8b2d8a53009357adfd38c78b8a0
Muammer ONER <muammer10er@gmail.com>
2024-02-08 20:57:17 +0100

    son

--- src/main/java/ugur.java
+++ src/main/java/ugur.java
@@ -1,7 +1,9 @@
 public class ugur {
     public static void main(String[] args) {
 
+        System.out.println("Ayni satirda yeni degisiklik");
         System.out.println("yeni degisiklik");
+        System.out.println("ilave degisiklik");
 
 
     }
